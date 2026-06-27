5c9040039d5558e2aacba6c323e05d3fefab0c4f
HM <108926387+HPMM2@users.noreply.github.com>
2026-06-26 21:20:56 -0600

    Delete api directory

--- api/__init__.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-import webbrowser
-from threading import Timer
-
-if __name__ == "__main__":
-    Timer(1.5, lambda: webbrowser.open("http://localhost:8000/docs")).start()
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
